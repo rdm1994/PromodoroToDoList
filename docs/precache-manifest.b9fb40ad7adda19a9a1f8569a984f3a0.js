d5aa52e5dd4f2c31f42fed5baa46c90b31ab1a70
Dim <russudim1994@gmail.com>
2019-09-07 18:54:18 +0400

    with prod build

---
+++ docs/precache-manifest.b9fb40ad7adda19a9a1f8569a984f3a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6548d836ccf21e72599189bd25319e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0122894e687168e30a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6c0f5f72a3661300b861",
+    "url": "/static/js/2.4647a6a6.chunk.js"
+  },
+  {
+    "revision": "5b0122894e687168e30a",
+    "url": "/static/js/main.fbdfdf19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
